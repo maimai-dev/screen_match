7adb6f48ee9ef7e7051d8fdf91b366cd9bc3b6fa
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2024-09-11 18:29:54 -0300

    commit 11/09

--- src/Principal.java
+++ src/Principal.java
@@ -1,16 +1,27 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme chefao = new Filme();
-        chefao.nome = "O poderoso chefão";
-        chefao.anoLancamento = 1970;
-        chefao.duracaoEmMinutos = 180;
+        chefao.setNome("O poderoso chefão");
+        chefao.setAnoLancamento(1970);
+        chefao.setDuracaoEmMinutos(180);
+        chefao.setIncluidoNoPlano(true);
 
         chefao.exibeFichaTecnica();
         chefao.avalia(8);
         chefao.avalia(5);
         chefao.avalia(10);
-        System.out.println(chefao.somaDasAvaliacoes);
-        System.out.println(chefao.totalDeAvaliacoes);
-        System.out.println(chefao.pegaMedia());
+        System.out.println("Total de avaliações: " + chefao.getTotalDeAvaliacoes());
+        System.out.println("Media de avaliações: " + chefao.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoLancamento(2010);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(10);
+
     }
 }
